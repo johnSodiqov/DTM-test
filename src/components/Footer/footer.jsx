f941b76a12c66c6ce2df3fe32512b3343a5bb696
Azizbek <ergasevaziz0611@gmail.com>
2023-05-27 17:52:47 +0500

    salom

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Footer/footer.css";
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Intro/navbar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import Logo from './img/Logo.png'
+const Navbar = () => {
+  const navbar = (
+    <nav className="navbar navbar-expand-lg navbar-light container-fluid ">
+      <div className="container">
+        <Link to='/'> <img src={Logo} alt="Lofo" className="Navbar_Logo" /> </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse flex-grow-0 navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <p className="nav-link active" aria-current="page">
+                Home
+              </p>
+            </li>
+            <li className="nav-item">
+              <p className="nav-link active" aria-current="page">
+                About us
+              </p>
+            </li>
+            <li className="nav-item">
+              <p className="nav-link active" aria-current="page">
+                Contact us
+              </p>
+            </li>
+            <li className="nav-item">
+                <button className="nav-link-button active" aria-current="page">
+                Login
+                </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+  return <div>
+    {navbar}
+  </div>;
+};
+
+export default Navbar;
