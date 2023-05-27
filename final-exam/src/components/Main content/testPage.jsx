bbcaec08e0b81154d445b8bdc8f0e3e7070f8b25
abdujalil <abdubannobovabdujalil@gmail.com>
2023-05-27 17:47:52 +0500

    yuklandi

--- final-exam/src/components/Main content/testPage.jsx
+++ final-exam/src/components/Main content/testPage.jsx
@@ -3,8 +3,19 @@ import "./testPage.css"
 
 const TestPage = () => {
     return (
-        <div>
-            
+        <div className='container'>
+            <div className="nav">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About us</a></li>
+                    <li><a href="">Contact us</a></li>
+                </ul>
+            </div>
+            <div className="main_card">
+                <div className="card">
+                    
+                </div>
+            </div>
         </div>
     );
 }
