7b1bafe07e845c752e841717e80bbbc3a4c48844
Azizbek <ergasevaziz0611@gmail.com>
2023-06-02 01:59:29 +0500

    salom

--- final-exam/src/components/Testing Page/testingPage.jsx
+++ final-exam/src/components/Testing Page/testingPage.jsx
@@ -4,19 +4,131 @@ import "./testingPage.css"
 import testData from '../Main content/testData';
 
 const TestingPage = () => {
-    const [amount, setAmount] = useState(0);
     const [testStatus, setTestStatus] = useState(false);
     const [tests, setTests] = useState([]);
-    console.log(amount);
     useEffect(() => {
         testData.getQuestion()
             .then((response) => {
+             
                 setTests(response)
             })
 
     }, []);
+    const testMath = (i , e) =>{
+        testData.getQuestion(i , e)
+        .then((response) => {
+            console.log(response);
+            setTests(response)
+        })
+        if(tests.length>0){
+            let current = [...tests]
+            
+            current.map(iteam =>{
+                let cur = iteam.correct_answer
+                if(iteam.incorrect_answers === cur){
+    
+                }
+                else{
+                    iteam.incorrect_answers.push(cur)
+                 
+                    let answers ={} 
+                    for(let key in answers){
+                        if(!answers.hasOwnProperty(cur[key])){
+                            answers[key] = cur[key]
+                        }
+                    }            
+                }
+            })
+            setTests(current)
+        }
+    }
+    
+    const testHistory = (i , e) =>{
+        testData.getQuestion(i , e)
+        .then((response) => {
+            setTests(response)
+        })
+        if(tests.length>0){
+            let current = [...tests]
+            
+            current.map(iteam =>{
+                let cur = iteam.correct_answer
+                if(iteam.incorrect_answers === cur){
+    
+                }
+                else{
+                    iteam.incorrect_answers.push(cur)
+                 
+                    let answers ={} 
+                    for(let key in answers){
+                        if(!answers.hasOwnProperty(cur[key])){
+                            answers[key] = cur[key]
+                        }
+                    }            
+                }
+            })
+            setTests(current)
+        }
+    }
+    
+    const testGeog = (i , e) =>{
+        testData.getQuestion(i , e)
+        .then((response) => {
+            setTests(response)
+        })
+        if(tests.length>0){
+            let current = [...tests]
+            
+            current.map(iteam =>{
+                let cur = iteam.correct_answer
+                if(iteam.incorrect_answers === cur){
+    
+                }
+                else{
+                    iteam.incorrect_answers.push(cur)
+                 
+                    let answers ={} 
+                    for(let key in answers){
+                        if(!answers.hasOwnProperty(cur[key])){
+                            answers[key] = cur[key]
+                        }
+                    }            
+                }
+            })
+            setTests(current)
+        }
+    }
+    
+    const testPoliticy = (i , e) =>{
+        testData.getQuestion(i , e)
+        .then((response) => {
+            setTests(response)
+        })
+        if(tests.length>0){
+            let current = [...tests]
+            
+            current.map(iteam =>{
+                let cur = iteam.correct_answer
+                if(iteam.incorrect_answers === cur){
+    
+                }
+                else{
+                    iteam.incorrect_answers.push(cur)
+                 
+                    let answers ={} 
+                    for(let key in answers){
+                        if(!answers.hasOwnProperty(cur[key])){
+                            answers[key] = cur[key]
+                        }
+                    }            
+                }
+            })
+            setTests(current)
+        }
+    }
+
     return (
-        <div className='row g-0'>
+        <div className='row g-0 TestSection'>
             <div className="col-3 side-bar">
                 <div className="accordion" id="accordionExample">
 
@@ -29,9 +141,9 @@ const TestingPage = () => {
                         <div id="collapseOne" className="accordion-collapse collapse " aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                             <div className="accordion-body ">
                                 <div className='mx-auto'>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>10</button>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>20</button>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>30</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { testMath(10, 19); setTestStatus(false) }}>10</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { testMath(20, 19); setTestStatus(false) }}>20</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { testMath(30, 19); setTestStatus(false) }}>30</button>
                                 </div>
                             </div>
                         </div>
@@ -46,9 +158,9 @@ const TestingPage = () => {
                         <div id="collapseTwo" className="accordion-collapse collapse " aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                             <div className="accordion-body ">
                                 <div className='mx-auto'>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>10</button>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>20</button>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>30</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { testHistory(10, 23); setTestStatus(false) }}>10</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => {  testHistory(20, 23); setTestStatus(false) }}>20</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => {  testHistory(30, 23); setTestStatus(false) }}>30</button>
                                 </div>
                             </div>
                         </div>
@@ -63,9 +175,9 @@ const TestingPage = () => {
                         <div id="collapseThree" className="accordion-collapse collapse " aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                             <div className="accordion-body ">
                                 <div className='mx-auto'>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>10</button>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>20</button>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>30</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { testGeog(10,22); setTestStatus(false) }}>10</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { testGeog(20,22); setTestStatus(false) }}>20</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { testGeog(30,22); setTestStatus(false) }}>30</button>
                                 </div>
                             </div>
                         </div>
@@ -80,46 +192,40 @@ const TestingPage = () => {
                         <div id="collapseFour" className="accordion-collapse collapse " aria-labelledby="headingFour" data-bs-parent="#accordionExample">
                             <div className="accordion-body ">
                                 <div className='mx-auto'>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>10</button>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>20</button>
-                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { setAmount(10); setTestStatus(false) }}>30</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { testPoliticy(10, 24); setTestStatus(false) }}>10</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { testPoliticy(20, 24); setTestStatus(false) }}>20</button>
+                                    <button className='btn btn-outline-warning mx-2 ' onClick={() => { testPoliticy(30, 24); setTestStatus(false) }}>30</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-
-
                 </div>
             </div>
-            <div className="col-9 test-area">
+            <div className="col-9 test-area ">
                 {
                     (!testStatus) ? <button className='btn start-button' onClick={() => { setTestStatus(true) }}>Get Started</button> :
-                        <div className='row overflow'>
-                            {
-                                tests.map((item, index1) => {
-                                    return (
-                                        <div key={index1} className="col-10">
-                                            <h3 className='text-dark'>{item.question}</h3>
-                                            {
-                                                item.answers.map((item, index2) => {
-                                                    return (
-                                                        <div key={index2} class="input-group">
-                                                            <div class="form-check">
-                                                                <input class="form-check-input" type="radio" name="flexRadioDefault" id={`flexRadioDefault${index1}`} value={item} />
-                                                                <label class="form-check-label" for={`flexRadioDefault${index1}`}>
-                                                                    {item}
-                                                                </label>
-                                                            </div>
-
-                                                        </div>
-                                                    )
-                                                })
-                                            }
-                                        </div>
-                                    )
-                                })
-                            }
+                        <div className='row overflow TestRow'>
+                              {
+                        (tests.length>0) && tests.map((iteam, index)=>{
+                            return(
+                                <div key={index} className="col-10 mt-2">
+                                    <h4  className='text-dark'>{iteam.question}</h4>
+                                    {
+                                        iteam.incorrect_answers.length>0 && iteam.incorrect_answers.map((iteam, index2)=>{
+                                            return(
+                                                <div key={index2} class="input-group">
+                                                   <input class="form-check-input" type="radio" name="flexRadioDefault" id={`flexRadioDefault${index}`} value={iteam} />
+                                                       <label class="form-check-label" for={`flexRadioDefault${index}`}>
+                                                      {iteam}
+                                                      </label>
+                                                </div>  
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                        })
+                    }
                         </div>
                 }
             </div>
@@ -129,3 +235,29 @@ const TestingPage = () => {
 }
 
 export default TestingPage;
+
+
+// {
+//     tests.map((item, index1) => {
+//         return (
+//             <div key={index1} className="col-10">
+//                 <h3 className='text-dark'>{item.question}</h3>
+//                 {
+//                     item.answers.map((item, index2) => {
+//                         return (
+//                             <div key={index2} class="input-group">
+//                                 <div class="form-check">
+//                                     <input class="form-check-input" type="radio" name="flexRadioDefault" id={`flexRadioDefault${index1}`} value={item} />
+//                                     <label class="form-check-label" for={`flexRadioDefault${index1}`}>
+//                                         {item}
+//                                     </label>
+//                                 </div>
+
+//                             </div>
+//                         )
+//                     })
+//                 }
+//             </div>
+//         )
+//     })
+// }
