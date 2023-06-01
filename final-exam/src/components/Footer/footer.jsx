d53082f395b817aa2649b8a09c7e38edf0865002
abdujalil <abdubannobovabdujalil@gmail.com>
2023-06-01 17:30:37 +0500

    ozgarish

--- final-exam/src/components/Footer/footer.jsx
+++ final-exam/src/components/Footer/footer.jsx
@@ -3,13 +3,14 @@ import "../Footer/footer.css";
 import {BsFacebook} from "react-icons/bs"
 import {FiTwitter} from "react-icons/fi"
 import {AiFillLinkedin} from "react-icons/ai"
+import rasm from './img/logo.png'
 
 const Footer = () => {
     return (
         <div className='container-fluid footer'>
             <div className="row">
                 <div className="col-4">
-                    <h1>LOGO</h1>
+                    <img src={rasm} alt="as" />
                     <p>sologon componiy</p>
                 </div>
                 <div className="col-3">
@@ -17,6 +18,8 @@ const Footer = () => {
                     <p>Home</p>
                     <p>About us</p>
                     <p>Contact us</p>
+                    <div>
+                </div>
                 </div>
                 <div className="col-3">
                     <h1>Contact</h1>
@@ -27,13 +30,13 @@ const Footer = () => {
                 </div>
                 <hr className='hrr'/>
                 <div className="row air">
-                    <div className="col-4">
+                    <div className="col-6">
                         <p>Copyright by Soff Study</p>
                     </div>
-                    <div className="col-6  con">
-                        <BsFacebook/>
-                        <FiTwitter/>
-                        <AiFillLinkedin/>
+                    <div className="col-6 con">
+                        <BsFacebook className='footer_icon'/>
+                        <FiTwitter className='footer_icon'/>
+                        <AiFillLinkedin className='footer_icon'/>
                     </div>
                 </div>
             </div>            
